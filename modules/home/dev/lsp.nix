29f551e3d8e69efcf7b5766850d0ee13580d0e77
blasmethian <dominako223@gmail.com>
2026-04-01 15:21:59 +0200

    fix: format

--- modules/home/dev/lsp.nix
+++ modules/home/dev/lsp.nix
@@ -5,16 +5,16 @@
     lua-language-server
     stylua
 
-    nodePackages.bash-language-server       # bashls
+    nodePackages.bash-language-server # bashls
     nodePackages.vscode-langservers-extracted # html, cssls, jsonls
     nodePackages.typescript-language-server # ts_ls
-    nodePackages.vim-language-server        # vimls
-    nodePackages.yaml-language-server       # yamlls
+    nodePackages.vim-language-server # vimls
+    nodePackages.yaml-language-server # yamlls
 
-    pyright            # pyright
-    rust-analyzer      # rust_analyzer
-    terraform-ls       # terraformls
-    marksman           # marksman
+    pyright # pyright
+    rust-analyzer # rust_analyzer
+    terraform-ls # terraformls
+    marksman # marksman
     nil
     nixpkgs-fmt
   ];
