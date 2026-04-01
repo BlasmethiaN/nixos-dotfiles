807460d438bb67cc11a6f40cf7909c66169ddbe8
blasmethian <dominako223@gmail.com>
2026-04-01 15:20:42 +0200

    feat: added lsp

--- modules/home/dev/lsp.nix
+++ modules/home/dev/lsp.nix
@@ -2,11 +2,19 @@
 
 {
   home.packages = with pkgs; [
-    # lua
     lua-language-server
     stylua
-    
-    # nix
+
+    nodePackages.bash-language-server       # bashls
+    nodePackages.vscode-langservers-extracted # html, cssls, jsonls
+    nodePackages.typescript-language-server # ts_ls
+    nodePackages.vim-language-server        # vimls
+    nodePackages.yaml-language-server       # yamlls
+
+    pyright            # pyright
+    rust-analyzer      # rust_analyzer
+    terraform-ls       # terraformls
+    marksman           # marksman
     nil
     nixpkgs-fmt
   ];
